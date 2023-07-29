cd170f2a72f27a8330e22c104c089651baa2502f
cchristina92 <130312075+cchristina92@users.noreply.github.com>
2023-07-28 22:22:03 -0700

    1st edit

--- WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "506053929051f9b4a10da60667ad5b36"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "0758df35457343d49be2fe95a49ce06d"
